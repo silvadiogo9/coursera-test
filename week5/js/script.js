72dedcf9331c2be19fc3af7585cf93d05cd8dace
silvadiogo9 <dass_9@hotmail.com>
2022-12-08 13:25:07 +0000

    new stuff

---
+++ week5/js/script.js
@@ -0,0 +1,183 @@
+// DOM manipulation
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+/*
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
+
+  // document
+  //   .getElementById("content")
+  //   .textContent = message;
+
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    title += " & Lovin' it!";
+    document
+        .querySelector("h1")
+        .textContent = title;
+  }
+} */
+
+/****************** Lecture 54 ******************/
+/*
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    function sayHello (event) {
+      this.textContent = "Said it!";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
+
+      document
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", sayHello);
+
+  }
+);
+
+
+
+// document.querySelector("button")
+//   .onclick = sayHello;
+
+*/
+
+/****************** Lecture 55 ******************/
+
+/*
+
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    function sayHello (event) {
+      console.log(event);
+
+      this.textContent = "Said it!";
+      var name =
+       document.getElementById("name").value;
+       var message = "<h2>Hello " + name + "!</h2>";
+
+      document
+        .getElementById("content")
+        .innerHTML = message;
+
+      if (name === "student") {
+        var title = 
+          document
+            .querySelector("#title")
+            .textContent;
+        title += " & Lovin' it!";
+        document
+            .querySelector("h1")
+            .textContent = title;
+      }
+    }
+
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", sayHello);
+
+    document.querySelector("body")
+      .addEventListener("mousemove",
+        function (event) {
+          if (event.shiftKey === true) {
+            console.log("x: " + event.clientX);
+            console.log("y: " + event.clientY);
+          }
+        }
+      );
+
+  }
+);
+
+*/
+
+/****************** Lecture 57 ******************/
+
+/*
+
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
+
+        
+      });
+  }
+);
+
+*/
+
+/****************** Lecture 58 ******************/
+
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesChineseFood) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+      });
+  }
+);
